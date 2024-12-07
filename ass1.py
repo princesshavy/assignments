97560afd05e3018a61331c991d7549bae73c1bfe
princesshavy <havilah757@gmail.com>
2024-12-07 20:31:14 +0100

    ass2

---
+++ ass1.py
@@ -0,0 +1,6 @@
+def add(num1: int, num2: int,num3:int,num4:int) -> int:
+ num1 = input()
+ num2 = input()
+ num3 = input()
+ num4 = input() 
+ print(num1+num2+num3+num4)
---
+++ ass2.py
@@ -0,0 +1,5 @@
+import math
+length= float(input("Input the length : "))
+width= float(input("Input the width : "))
+area = length*width
+print("Area of the circle:", area)
