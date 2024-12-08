360aefdf20a1e4fb787b432c7acb126c21a9598e
princesshavy <havilah757@gmail.com>
2024-12-08 18:33:34 +0100

    my bad

--- ass6.py
+++ ass6.py
@@ -1,7 +1,2 @@
-# um = int (input ("Enter your chosen number to check whether it is odd or even: ") )
-#if (num % 2) == 0:
-  #  print ("The number is even") 
-#else:
- #   print ("It is odd")  
 txt = str (input ("Enter your preferred text to reverse :"))
 print(txt[::-1])
