52872ac24e92702b092b2e66aae147f332503075
princesshavy <havilah757@gmail.com>
2024-12-08 16:41:50 +0100

    ass6

--- ass6.py
+++ ass6.py
@@ -1,4 +1,7 @@
-def my_function(txt):
-  return txt[::-1]
-
-print("Type your preferred text:")
+# um = int (input ("Enter your chosen number to check whether it is odd or even: ") )
+#if (num % 2) == 0:
+  #  print ("The number is even") 
+#else:
+ #   print ("It is odd")  
+txt = str (input ("Enter your preferred text to reverse :"))
+print(txt[::-1])
