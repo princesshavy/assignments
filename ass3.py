11c036d017221a4efe62180d8b2ff747543c3165
princesshavy <havilah757@gmail.com>
2024-12-08 16:31:35 +0100

    ass3

---
+++ ass3.py
@@ -0,0 +1,5 @@
+num = int (input ("Enter your chosen number to check whether it is odd or even: ") )
+if (num % 2) == 0:
+    print ("The number is even") 
+else:
+    print ("It is odd") 
---
+++ ass6.py
@@ -0,0 +1,4 @@
+def my_function(txt):
+  return txt[::-1]
+
+print("Type your preferred text:")
